ecf738696c84d863d7a3dd9edc4a6058c8418770
Tucker <tudge669@gmail.com>
2022-11-01 11:32:41 -0500

    commit tree f416d88c43d87fb0949eb38bb9a727526a9e1445

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(num,num2){
+return num + num2
+}
+function subtract(num,num2){
+return num - num2
+}
+function multiply(num,num2){
+    return num * num2
+}
+function divide(num,num2){
+    return num / num2
+}
+function increment(n = 1){
+    return ++n
+}
+function decrement(n = 1){
+    return --n
+}
+function makeInt (n){
+    return parseInt(n,10)
+}
+function preserveDecimal(n){
+    return parseFloat(n,10)
+}
